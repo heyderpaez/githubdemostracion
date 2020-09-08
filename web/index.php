4fcfd1cd85763ae6f9a9bbe4a2a1bcc42138e22c
heyderpaez <heyderpaez@gmail.com>
2020-09-07 19:13:42 -0500

    Prueba de envío y visualización de un dato en pantalla

--- web/index.php
+++ web/index.php
@@ -22,4 +22,9 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/enviarDato/{dato}', function($dato) use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return echo $dato;
+});
+
 $app->run();
