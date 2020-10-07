23b4f7efad5d890ff62d0f37759b7f17c69fecf4
heyderpaez <heyderpaez@gmail.com>
2020-10-06 20:49:13 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -40,7 +40,7 @@ $app->post('/guardarDato', function (Request $request) use ($app) {
 	$dbconn = pg_pconnect("host=ec2-52-21-0-111.compute-1.amazonaws.com port=5432 dbname=da23ojrg1de3ae user=msmhlrvxhgltyv password=baf2024024b59cdd7b5bd1a44e8d8a7773810a5ccbce3719f01225c9baac9bf2");
 
 	$data = array(
-		"fecha" => date("d-m-Y H:i"),
+		"fecha"=>date('Y-m-d H:i:s'),
 		"temperature" => $temperature
 		);
 
