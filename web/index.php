b765a5526facd03404ab4e8ffc4105e6ae5260bc
heyderpaez <heyderpaez@gmail.com>
2020-10-13 19:28:13 -0500

    Nueva conexion a tabla de consumo

--- web/index.php
+++ web/index.php
@@ -51,6 +51,27 @@ $app->post('/guardarDato', function (Request $request) use ($app) {
    	return $respuesta;
 });
 
+$app->post('/guardarConsumo', function (Request $request) use ($app) {
+
+	$corriente = $request->get('corriente');
+	$voltaje = $request->get('voltaje');
+	$tabla = $request->get('tabla');
+	$lugar = $request->get('lugar');
+
+	$dbconn = pg_pconnect("host=ec2-52-21-0-111.compute-1.amazonaws.com port=5432 dbname=da23ojrg1de3ae user=msmhlrvxhgltyv password=baf2024024b59cdd7b5bd1a44e8d8a7773810a5ccbce3719f01225c9baac9bf2");
+
+	$data = array(
+		"fecha"=>date('Y-m-d H:i:s'),
+		"corriente" => $corriente,
+		"voltaje" => $voltaje,
+		"lugar" => $lugar
+		);
+
+	$respuesta = pg_insert($dbconn, $tabla, $data);
+   	
+   	return $respuesta;
+});
+
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
 $app->post('/postArduino', function (Request $request) use ($app) {
    	return "OK";
