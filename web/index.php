7121a05533ae6a13d88e4090cca1cf6f7367d97f
heyderpaez <heyderpaez@gmail.com>
2020-10-20 18:42:06 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -76,7 +76,7 @@ $app->post('/guardarConsumo', function (Request $request) use ($app) {
 $app->get('/consultarDatos', function () use ($app) {
 
 	$dbconn = pg_pconnect("host=ec2-52-21-0-111.compute-1.amazonaws.com port=5432 dbname=da23ojrg1de3ae user=msmhlrvxhgltyv password=baf2024024b59cdd7b5bd1a44e8d8a7773810a5ccbce3719f01225c9baac9bf2");
-	$query = "SELECT * FROM clima_house WHERE id>30;";
+	$query = "SELECT * FROM clima_house ORDER BY id DESC LIMIT 15";
 
 	$consulta = pg_query($dbconn, $query);
 
