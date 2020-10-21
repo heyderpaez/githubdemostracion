086d9b40abed5ec6e612856cba9bc009041dcc6a
heyderpaez <heyderpaez@gmail.com>
2020-10-20 19:58:10 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -116,6 +116,22 @@ $app->get('/consultarDatos', function () use ($app) {
 	return "OK";
 });
 
+$app->get('/limpiarDatos', function () use ($app) {
+
+	$dbconn = pg_pconnect("host=ec2-52-21-0-111.compute-1.amazonaws.com port=5432 dbname=da23ojrg1de3ae user=msmhlrvxhgltyv password=baf2024024b59cdd7b5bd1a44e8d8a7773810a5ccbce3719f01225c9baac9bf2");
+	
+	$query_last = "SELECT * FROM clima_house ORDER BY id DESC LIMIT 1";
+	$query_first = "SELECT * FROM clima_house ORDER BY id ASC LIMIT 1";
+
+	$consulta_last = pg_query($dbconn, $query_last);
+	$consulta_first = pg_query($dbconn, $query_first);
+
+	$id_last = pg_fetch_result($consulta_last, null, 0);
+	$id_first = pg_fetch_result($consulta_first, null, 0);
+
+	return $id_last - $id_first;
+});
+
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
 $app->post('/postArduino', function (Request $request) use ($app) {
    	return "OK";
