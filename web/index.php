bb04822e96575144c0320816a448d788bfc248a1
heyderpaez <heyderpaez@gmail.com>
2020-11-03 17:54:56 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -128,7 +128,7 @@ $app->get('/consultarDatos', function () use ($app) {
 $app->get('/getDataGoogle', function () use ($app) {
 
 	$dbconn = pg_pconnect("host=ec2-52-21-0-111.compute-1.amazonaws.com port=5432 dbname=da23ojrg1de3ae user=msmhlrvxhgltyv password=baf2024024b59cdd7b5bd1a44e8d8a7773810a5ccbce3719f01225c9baac9bf2");
-	$query = "SELECT * FROM climahouse ORDER BY id DESC LIMIT 15";
+	$query = 'SELECT "fecha", "consumo" FROM climahouse ORDER BY id DESC LIMIT 15';
 
 	$consulta = pg_query($dbconn, $query);
 
